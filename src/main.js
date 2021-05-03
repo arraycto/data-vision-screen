d12a5adebf4cb4d97f98381d3dd3bb12aa178ca8
SherwinShen <306407158@qq.com>
2021-05-03 18:26:15 +0800

    chore: 初始化项目

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import DataV from 'data-vision-libs'
+import ECharts from 'vue-echarts'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(DataV)
+  .component('v-chart', ECharts)
+  .mount('#app')
